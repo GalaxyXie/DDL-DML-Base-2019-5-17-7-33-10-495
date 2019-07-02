ec9fe002dbbcaaa9a266cc68a8e9242c00311091
GalaxyXie <771472141@qq.com>
2019-07-02 18:00:25 +0800

    update dml.sql

--- dml.sql
+++ dml.sql
@@ -1,7 +1,8 @@
 -- 插入记录  
-
+INSERT INTO table_name (列1, 列2,...) VALUES (值1, 值2,....)
 -- 修改记录  
-
+UPDATE table_name SET 列名称 = 新值 WHERE 列名称 = 某值
 --  删除记录  
-
+DELETE FROM table_name WHERE 列名称 = 值
 -- 查询记录  
+SELECT 列名称 FROM table_name
